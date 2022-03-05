abb17b69fae90d1c86aff2196f5b7e9b38764d64
Saed Sanatnegar <sanatnegar@gmail.com>
2022-03-05 18:22:41 +0330

    Register admin blueprint to main app

--- app.py
+++ app.py
@@ -7,3 +7,11 @@ app = Flask(__name__)
 def index():
     return "Blog Home."
 
+
+from mod_admin import admin
+app.register_blueprint(admin)
+
+
+if __name__ == '__main__':
+    app.secret_key='secret123'
+    app.run(debug=True)
